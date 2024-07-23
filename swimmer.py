4d8c92e4e29779d7766023fc1f971da05e07630a
jyotirmay-neuro <101052218+jyotirmay-neuro@users.noreply.github.com>
2024-07-23 20:07:26 +0530

    Update swimmer.py
    Update for Food_Swimmer task

--- swimmer.py
+++ swimmer.py
@@ -214,3 +214,47 @@ class Swimmer(base.Task):
                              bounds=(0, target_size),
                              margin=5*target_size,
                              sigmoid='long_tail')
+    
+class Food_Swimmer(control.Task):
+    def __init__(self, arena_size=(1, 1)):
+        self.arena_size = arena_size
+        self.food_zone_size = min(arena_size) / 10
+        self.food_zone_pos = self._random_food_zone()
+        self.sensor_model = FFNSensor()
+        self.optimizer = optim.Adam(self.sensor_model.parameters(), lr=0.001)
+        super().__init__()
+
+    def _random_food_zone(self):
+        x = np.random.uniform(-self.arena_size[0] / 2 + self.food_zone_size / 2,
+                              self.arena_size[0] / 2 - self.food_zone_size / 2)
+        y = np.random.uniform(-self.arena_size[1] / 2 + self.food_zone_size / 2,
+                              self.arena_size[1] / 2 - self.food_zone_size / 2)
+        return np.array([x, y])
+
+    def initialize_episode(self, physics):
+        super().initialize_episode(physics)
+        self.food_zone_pos = self._random_food_zone()
+        physics.named.data.geom_xpos['food_zone'][:2] = self.food_zone_pos
+
+    def get_observation(self, physics):
+        obs = {}
+        obs['position'] = physics.named.data.geom_xpos['nose'][:2]
+        obs['food_zone'] = self.food_zone_pos
+        obs['smell_strength'] = self._smell_strength(obs['position'])
+        obs['joints'] = physics.joints()
+        obs['to_target'] = physics.nose_to_target()
+        obs['body_velocities'] = physics.body_velocities()
+        return obs
+
+    def _smell_strength(self, position):
+        distance = np.linalg.norm(position - self.food_zone_pos)
+        return np.exp(-distance)
+
+    def get_reward(self, physics):
+        position = physics.named.data.geom_xpos['nose'][:2]
+        smell_strength = self._smell_strength(position)
+        vel = physics.body_velocities()
+        reward = smell_strength + 0.1 * vel
+        if np.linalg.norm(position - self.food_zone_pos) > self.food_zone_size:
+            reward -= 0.1 * np.exp(np.linalg.norm(position - self.food_zone_pos))
+        return reward
